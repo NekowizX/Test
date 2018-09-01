175ac80d72cd21a0fb9d984f3c9a1d868ab5f24b
NekowizX <31812529+NekowizX@users.noreply.github.com>
2018-09-01 17:42:27 +0800

    Update wiz.js

--- wiz.js
+++ wiz.js
@@ -103,11 +103,12 @@ wizLoader = (function() {
   };
 
   wizLoader._loadNormal = function(indata) {
-    var col, data, db, entry, index, keys, name, tmp;
+    var col, data, db, entry, index, keys, name, tmp, xcount;
     data = indata[0];
     name = indata[1];
     tmp = {};
     col = 0;
+    xcount = 0;
     keys = ['', '', 'question', 'answer'];
     db = [];
     for (index in data) {
@@ -129,7 +130,8 @@ wizLoader = (function() {
           db.push(tmp);
         }
       }
-      $("#loaded-count").text(this.data.db().count() + '///' + Object.keys(this.option.excelIds).length);
+      xcount++;
+      $("#loaded-count").text(xcount + '///' + Object.keys(this.option.excelIds).length);
     }
     wizLoader.data.db.insert(db);
     return this._loadComplete();
@@ -142,7 +144,7 @@ wizLoader = (function() {
       $("#load-count").text('共讀取 ' + this.data.db().count() + ' 個問題。');
       $("#result-limit").html("<span class='hidden-xs'>僅顯示</span>前 <a href='#' data-toggle='modal' data-target='#setting-modal'>" + (Setting.get('searchMaxResult')) + " </a>個<span class='hidden-xs'>結果</span>。");
     } else {
-      $("#loaded-count").text(this.loadCount + '///' + Object.keys(this.option.excelIds).length);
+      $("#loaded-count").text(this.data.db().count() + '///' + Object.keys(this.option.excelIds).length);
     }
   };
 
