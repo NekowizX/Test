392a63cb3f89ebdb8406ff87dd201e318ef8d281
NekowizX <31812529+NekowizX@users.noreply.github.com>
2018-09-01 17:52:53 +0800

    Update wiz.js

--- wiz.js
+++ wiz.js
@@ -141,11 +141,10 @@ wizLoader = (function() {
     this.loadCount++;
     if (this.loadCount === Object.keys(this.option.excelIds).length) {
       $("#overlay-loading").remove();
-     // $("#load-count").text('共讀取 ' + this.data.db().count() + ' 個問題。');
-      $("#load-count").text('共讀取 ' + xcount + ' 個問題。');
+      $("#load-count").text('共讀取 ' + this.data.db().count() + ' 個問題。');
       $("#result-limit").html("<span class='hidden-xs'>僅顯示</span>前 <a href='#' data-toggle='modal' data-target='#setting-modal'>" + (Setting.get('searchMaxResult')) + " </a>個<span class='hidden-xs'>結果</span>。");
     } else {
-//      $("#loaded-count").text(this.data.db().count() + '///' + Object.keys(this.option.excelIds).length);
+      $("#loaded-count").text(this.data.db().count() + '///' + Object.keys(this.option.excelIds).length);
     }
   };
 
